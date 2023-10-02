9aa6f40e8af089e300026100b6c3873d87e9cf3b
Connie Leung <cleung334@gmail.com>
2023-10-03 00:58:34 +0800

    feat: add services

---
+++ app.js
@@ -0,0 +1,28 @@
+const $ = function(args){ return document.querySelector(args);}
+const $$ = function(args){ return document.querySelectorAll(args);}
+
+HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
+HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
+HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
+HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
+
+import Store from './services/Store.js';
+import API from './services/API.js';
+import { loadData } from './services/Order.js';
+import Router from './services/Router.js';
+
+window.app = {};
+app.store = Store;
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", async () => {
+    app.router.init();
+    await loadData();
+});
+
+function renderTest() {
+    const categories = app.store.menu.map((item) => item.name);
+    console.log('categories', categories);
+}
+
+window.renderTest = renderTest;
---
+++ services/Router.js
@@ -0,0 +1,48 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll('a.navlink').forEach((a) => {
+            a.addEventListener('click', (event) => {
+                event.preventDefault();
+                const href = event.target.getAttribute('href');
+                Router.go(href);
+            })
+        });
+
+        window.addEventListener('popstate', (event) => {
+            console.log('event', event);
+            Router.go(event.state.route, false);
+        });
+    },
+    go: (route, addToHistory = true) => {
+        if (addToHistory) {
+            history.pushState({ route }, '', route);
+        }
+
+        let pageElement = null;
+        switch (route) {
+            case '/':
+                pageElement = document.createElement('h1');
+                pageElement.textContent = 'Menu';
+                break;
+            case '/order':
+                pageElement = document.createElement('h1');
+                pageElement.textContent = 'Order';
+                break;
+            default: 
+                if (route.startsWith("/product-")) {                
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
+                }
+                break;
+        }
+        if (pageElement) {
+            document.querySelector('main').innerHTML = '';
+            document.querySelector('main').appendChild(pageElement);
+        }
+
+        window.scrollX = 0;
+    }
+}
+
+export default Router;
